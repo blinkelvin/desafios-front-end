d4038044018a4b7f787f0ffad8faa377a7758277
Kelvin Silva de Medeiros <kelvin.medeiros@mstech.com.br>
2016-08-24 13:25:16 -0300

    Desafio I de Angular Finalizado

--- AngularCRUD/assets/app/app.js
+++ AngularCRUD/assets/app/app.js
@@ -1,31 +1,49 @@
-var data = []
-var app = angular.module('ToDo', []);
+
+var data = [];
+
+var situation =
+				[
+					{"id":1, "nome": "Finalizada"},
+					{"id":2, "nome": "Pendente"}
+				];
+
+if (localStorage.getItem('todo') != null) {
+	data = JSON.parse(localStorage.getItem('todo'));
+}
+if (localStorage.getItem('sit') != null) {
+	situation = JSON.parse(localStorage.getItem('sit'));
+}
+//localStorage.clear();
 
 function generateUUID() {
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-    });
-    return uuid;
+	var d = new Date().getTime();
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = (d + Math.random()*16)%16 | 0;
+		d = Math.floor(d/16);
+		return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+	});
+	return uuid;
 };
 
 function findIndexByKeyValue(arraytosearch, key, valuetosearch) {
+	for (var i = 0; i < arraytosearch.length; i++) {
+		if (arraytosearch[i][key] == valuetosearch) {
+			console.log(arraytosearch[i][key]);
+			return i;
+		};
+	};
+	return null;
+};
 
-    for (var i = 0; i < arraytosearch.length; i++) {
-
-        if (arraytosearch[i][key] == valuetosearch) {
-            console.log(arraytosearch[i][key]);
-            return i;
-        }
-    }
-    return null;
-}
+var app = angular.module('ToDo', []);
 
 app.controller('ToDoController',function($scope) {
     $scope.setData = {};
     $scope.setData.situation = "Finalizada";
+
+	$scope.setSituation = {};
+
+	$scope.getSituation = situation;
 
 // C
     $scope.add = function() {
@@ -38,6 +56,7 @@ app.controller('ToDoController',function($scope) {
             $scope.setData.id = generateUUID();
             data.push($scope.setData);
         }
+        localStorage.setItem('todo', JSON.stringify(data));
 
         $scope.setData = {};
         $scope.setData.situation = "Finalizada";
@@ -56,9 +75,35 @@ app.controller('ToDoController',function($scope) {
     $scope.delete = function(id) {
         var i = findIndexByKeyValue(data,"id",id);
         data.splice(i, 1);
+        localStorage.setItem('todo', JSON.stringify(data));
     };
 
-    $scope.finalizadas = function(e) {
-        console.log(e);
-    }
+	$scope.addSit = function() {
+		$scope.setSituation.id = generateUUID();
+		situation.push($scope.setSituation);
+		$scope.setSituation = {};
+        localStorage.setItem('sit', JSON.stringify(situation));
+	};
+
+    $scope.deleteSit = function(id) {
+        var i = findIndexByKeyValue(situation,"id",id);
+        data.splice(i, 1);
+        localStorage.setItem('sit', JSON.stringify(situation));
+    };
+
+    $scope.filter = function() {
+
+    };
+	$scope.getStatus = function(status) {
+		$scope[status+"GetStatus"] = 0;
+		$.map(data, function(val, i) {
+			val.situation == status ? $scope[status+"GetStatus"]++ : null;
+		});
+		return $scope[status+"GetStatus"];
+	};
+
+	//console.log($scope.getStatus("Finalizada"));
+	//console.log($scope.getStatus("Pendente"));
+
+	console.log($scope.FinalizadaGetStatus);
 });
