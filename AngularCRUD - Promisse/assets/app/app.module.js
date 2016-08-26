555ed89b7e3d8d050f6db8cb78172f6124366e31
Kelvin Silva de Medeiros <kelvin.medeiros@mstech.com.br>
2016-08-26 14:26:59 -0300

    Angular CRUD com arquitetura

---
+++ AngularCRUD - Promisse/assets/app/app.module.js
@@ -0,0 +1,11 @@
+/**
+ * @author MStech - 01/07/2016
+ * */
+angular
+  .module("ToDo", [
+    "app.controllers",
+    "app.services",
+    "app.directivies",
+    "app.routes",
+    "app.filters"
+  ]);
